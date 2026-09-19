c7eae08232e270d42969268203439fc9940200e9
emiresh <ireshek@gmail.com>
2026-09-19 19:52:50 +0530

    updated

--- src/routes/thank-you.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/thank-you")({
-  beforeLoad: () => {
-    throw redirect({ to: "/thankyou" });
-  },
-  component: () => null,
-});
